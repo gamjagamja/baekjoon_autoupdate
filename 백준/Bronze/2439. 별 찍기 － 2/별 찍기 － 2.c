50ed34c178b79dd60f8fbec2b499be224d5c2dd1
gamjagamja <114169921+gamjagamja@users.noreply.github.com>
2022-10-02 14:15:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    int star_n, i,j;
+    int star_n, i, j;
     scanf("%d", &star_n);
     for(i = 1; i <= star_n; i++)
     {
-        for(j = 1; j <= star_n - i; j++)
-            printf(" ");
+       for(j = 1; j <= star_n - i; j++)
+           printf(" ");
         for(j = 1; j <= i; j++)
             printf("*");
         printf("\n");
