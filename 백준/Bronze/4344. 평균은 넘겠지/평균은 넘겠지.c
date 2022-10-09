e0bf73b58ce0bd3d75bd591e102aadd730aa401a
gamjagamja <114169921+gamjagamja@users.noreply.github.com>
2022-10-09 18:33:22 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int c = 0;
+
+  
+    scanf("%d\n", &c);
+    for(int i =0; i < c; i++)
+     {
+        int n = 0;
+        scanf("%d", &n);
+        int score[n];
+        int sum = 0;
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%d", &score[j]);
+            sum += score[j];
+        }
+        float average = 0.00;
+        average = (double)sum/n;
+
+        int count = 0;
+        for(int k = 0; k < n; k++)
+        {
+            if(score[k] > average)
+                count++;
+        }
+       
+        printf("%.3f%%\n", ((double)count/n)*100);
+        
+     }
+    return 0;
+}
